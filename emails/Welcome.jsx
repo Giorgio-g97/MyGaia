dab6de0b322ef94d485d5fdb026295211cb2a9f3
Giorgio-g97 <giorgio.g97@gmail.com>
2024-09-11 19:34:55 +0200

    Redesign Welcome Mail

--- emails/Welcome.jsx
+++ emails/Welcome.jsx
@@ -1,68 +1,104 @@
 import {
+  Heading,
   Body,
   Button,
   Container,
-  Column,
   Head,
-  Heading,
   Hr,
   Html,
   Img,
-  Link,
   Preview,
-  Row,
   Section,
   Text,
   Tailwind,
 } from "@react-email/components";
 import * as React from "react";
 
-export default function Welcome({ nomeCliente, nomeOperatore, dataApp, ora}) {
+export default function Welcome({ nomeCliente, nomeOperatore, dataApp, ora }) {
+  const main = {
+    backgroundColor: "#ffffff",
+    fontFamily:
+      '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif',
+  };
+
+  const container = {
+    margin: "0 auto",
+    padding: "20px 0 48px",
+  };
+
+  const logo = {
+    margin: "0 auto",
+  };
+
+  const paragraph = {
+    fontSize: "16px",
+    lineHeight: "26px",
+  };
+
+  const btnContainer = {
+    textAlign: "center",
+  };
+
+  const button = {
+    backgroundColor: "#5F51E8",
+    borderRadius: "3px",
+    color: "#fff",
+    fontSize: "16px",
+    textDecoration: "none",
+    textAlign: "center",
+    display: "block",
+    padding: "12px",
+  };
+
+  const hr = {
+    borderColor: "#cccccc",
+    margin: "20px 0",
+  };
+
+  const footer = {
+    color: "#8898aa",
+    fontSize: "12px",
+  };
+
   return (
     <Html>
       <Head />
-      <Preview>Prenotazione appuntamento</Preview>
+      <Preview>Prenotazione Appuntamento</Preview>
       <Tailwind>
-        <Body className="bg-white my-auto mx-auto font-sans px-2">
-          <Container className="border border-solid border-[#eaeaea] rounded my-[40px] mx-auto p-[20px] max-w-[465px]">
-            <Section className="mt-[32px] flex justify-center items-center">
-              <Img
-                className="flex items-center justify-center text-center"
-                src="https://my-gaia.vercel.app/_next/image?url=%2Flogo.jpg&w=256&q=75"
-                width={200}
-                height={100}
-                alt="logo"
-              />
-            </Section>
+        <Body style={main}>
+          <Container style={container} className="shadow-xl rounded-md">
+            <Img
+              src="https://my-gaia.vercel.app/_next/image?url=%2Flogo.jpg&w=256&q=75"
+              width="200"
+              height="100"
+              alt="logo"
+              style={logo}
+            />
             <Heading className="text-black text-[24px] font-normal text-center p-0 my-[30px] mx-0">
-              Prenotazione appuntamento con {nomeCliente}
+              Prenotazione appuntamento con <strong>{nomeCliente}</strong>
             </Heading>
-            <Text className="text-black text-[14px] leading-[24px]">
+            <Text className="p-5" style={paragraph}>
               Ciao {nomeOperatore},
             </Text>
-            <Text className="text-black text-[14px] leading-[24px]">
-              Il cliente {nomeCliente} ha prenotato con te un appuntamento, di
-              seguito i dettagli:
-              <p>
-                <strong>Data appuntamento: </strong>
-                {dataApp}
-              </p>
-              <p>
-                <strong>Ora: </strong> {ora}
-              </p>
+            <Text className="p-5" style={paragraph}>
+              Il cliente <strong>{nomeCliente}</strong> ha prenotato con te un
+              appuntamento, di seguito i dettagli:
             </Text>
-            <Section></Section>
-            <Section className="text-center mt-[32px] mb-[32px]"></Section>
-            <Hr className="border border-solid border-[#eaeaea] my-[26px] mx-0 w-full" />
-            <Text className="text-[#666666] text-[12px] leading-[24px]">
-              This invitation was intended for{" "}
-              <span className="text-black"></span>. This invite was sent from{" "}
-              <span className="text-black"></span> located in{" "}
-              <span className="text-black"></span> Se non si aspettava questo
-              invito, si può ignorare questa e-mail. Se se si è preoccupati per
-              la sicurezza del proprio account, si prega di rispondere a questa
-              e-mail "(domain email)" per mettervi in contatto con noi.
-            </Text>
+            <Section style={btnContainer}>
+              <Text>
+                <p className="text-left p-5">
+                  <strong>Data appuntamento: </strong>
+                  {dataApp}
+                </p>
+                <p className="text-left p-5">
+                  <strong>Ora: </strong> {ora}
+                </p>
+              </Text>
+              <Hr style={hr} />
+              <Text className="text-slate-600 text-left p-5">
+                Se non l'avete richiesto, ignorate questa e-mail.
+              </Text>
+            </Section>
           </Container>
         </Body>
       </Tailwind>
